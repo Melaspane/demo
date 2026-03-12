8e3a1ddf3e409fde606d95bb9bf0a0a76200345d
Mohamed Rashad <122336017+Melaspane@users.noreply.github.com>
2026-03-12 02:30:49 +0200

    Fix typo in import statement for userEffect

--- frontend/src/Shop/index.tsx
+++ frontend/src/Shop/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,userEffect} from 'react';
 import axios from 'axios';
 import ProductCard from './components/ProductCard';
 import SignIn from './components/SignIn';
