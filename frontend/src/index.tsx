ef12d69555fb377d10bb3e3459e4457b718e24d7
Mohamed Rashad <122336017+Melaspane@users.noreply.github.com>
2026-03-14 20:55:51 +0200

    Update CSS import and initialize Pi on load

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
+import './normalize.css';
 import './defaults.css';
 import Shop from './Shop';
+
+if (window.Pi) {
+  window.Pi.init({ version: "2.0", sandbox: true });
+}
 
 ReactDOM.render(
   <React.StrictMode>
